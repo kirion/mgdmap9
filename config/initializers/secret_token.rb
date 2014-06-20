1a0ef93519998d0efb82cc28dfa1bfe0cd4bd50a
Kirion <ysergeev@gmail.com>
2014-06-20 10:04:56 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mgdmap9::Application.config.secret_token = 'c907d2d419fbe0fa0d1bfc608066ae3a580b96940ef09500e8ee95fb5dd8fd8bce84c184a7ea127ea01e2e6bec64e28d7c2cb47ebf7f3f2c3947cc5f744b1e68'
